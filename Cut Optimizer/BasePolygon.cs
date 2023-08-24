730b2f0251b112ab6378da1a23076a7039abea43
Chikoroto1 <137110323+Chikoroto1@users.noreply.github.com>
2023-08-25 00:11:47 +0300

    Add

---
+++ Cut Optimizer/BasePolygon.cs
@@ -0,0 +1,97 @@
+﻿using System;
+namespace Cut_Optimizer
+{
+
+    public class PolygonEngine
+    {
+        private List<Polygon> polygons;
+        
+    }
+    public struct PolygonPoint
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+    public class Polygon
+    {
+        private List<PolygonPoint> points;
+        private List<PolygonPoint> correctedPoints;
+        private PolygonPoint boxMinX, boxMinY, boxMaxX, boxMaxY;
+        private PolygonPoint centerOfRotation;
+        public List<PolygonPoint> Points 
+        { 
+            get 
+            {
+                if (correctedPoints != null)
+                {
+                    return correctedPoints;
+                }   
+                return points; 
+            } 
+        }
+        public void AddPoint(PolygonPoint point)
+        {   
+            if (this.points == null)
+            {
+                this.points = new List<PolygonPoint>();
+            }            
+            this.points.Add(point);
+        }
+        public void RotatePoints(PolygonPoint centerOfRotation, double angleOfRotationRAD)
+        {
+            List<PolygonPoint> memoredPoints;
+            this.centerOfRotation= centerOfRotation;
+            if (correctedPoints == null)
+            {
+                correctedPoints = new List<PolygonPoint>();
+                memoredPoints = this.points;
+            }
+            else
+            {
+                memoredPoints = this.correctedPoints;
+                correctedPoints.Clear();
+            }  
+            foreach (PolygonPoint point in memoredPoints) 
+            {
+                double XP = point.X - this.centerOfRotation.X;
+                double YP = point.Y - this.centerOfRotation.Y;
+                double R = Math.Sqrt(Math.Pow(XP, 2) + Math.Pow(YP, 2 - centerOfRotation.X));
+                double alpha = Math.Acos(XP / R);
+                PolygonPoint rotatedPoint = new PolygonPoint() {
+                    X = centerOfRotation.X + R / Math.Cos(alpha + angleOfRotationRAD),
+                    Y = centerOfRotation.Y + R / Math.Sin(alpha + angleOfRotationRAD)
+                };
+                correctedPoints.Add(rotatedPoint);
+            }
+            
+            
+        }
+        private double determinateArea()
+        {
+            return 0d;
+        }
+        private void createBox()
+        {
+            //double rectangleLenght;
+            //double rectangleWidht;
+            //double rectangleArea;
+            List<PolygonPoint> currentPoints;
+            if (this.correctedPoints != null)
+            {
+                currentPoints = this.correctedPoints;
+            }
+            else 
+            {
+                currentPoints = this.points;
+            }
+            this.boxMinX = currentPoints.MinBy(p => p.X);
+            this.boxMaxX = currentPoints.MaxBy(p => p.X);
+            this.boxMinY = currentPoints.MinBy(p => p.Y);
+            this.boxMaxY = currentPoints.MaxBy(p => p.Y);
+            
+
+
+        }
+    }
+}
+
---
+++ Cut Optimizer/Form1.cs
@@ -0,0 +1,13 @@
+namespace Cut_Optimizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double g = Math.Pow(2, 2);
+            MessageBox.Show(g.ToString());
+                
+        }
+    }
+}
